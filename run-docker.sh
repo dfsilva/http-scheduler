60e1b87394100d022351b02f378b2695c9e13143
Diego <diego@diegosilva.com.br>
2021-02-03 14:43:08 -0300

    fixes

--- run-docker.sh
+++ run-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run \
   --name http-scheduler \
-  -e JDBC_URL="jdbc:postgresql://sm-database.cd2fkii9d6sk.sa-east-1.rds.amazonaws.com:5432/postgres?currentSchema=http_sched" \
+  -e JDBC_URL="jdbc:postgresql://your_database:5432/postgres?currentSchema=http_sched" \
   -e JDBC_USER="postgres" \
   -e JDBC_PASSWORD="postgres" \
   -p 8181:8181 \
