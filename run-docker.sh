e1291523735f7c5723ab5c31067bc8ffbc14eb52
Diego <diego@diegosilva.com.br>
2021-02-03 21:21:40 -0300

    fixes

--- run-docker.sh
+++ run-docker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 docker run \
   --name http-scheduler \
-  -e JDBC_URL="jdbc:postgresql://your_database:5432/postgres?currentSchema=http_sched" \
+  -e JDBC_URL="jdbc:postgresql://database_host:5432/postgres?currentSchema=http_sched" \
   -e JDBC_USER="postgres" \
   -e JDBC_PASSWORD="postgres" \
-  -p 8181:8181 \
+  -e INITIALIZE_SCHEMA="never" \
   -d \
+  -p 8181:8181 \
   diego/http-scheduler
 
